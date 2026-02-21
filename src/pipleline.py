df1cb8f78a3280b3e540bfe9f445aecc72133bd3
Jimmy Warthon <jim.warthon@gmail.com>
2026-02-21 15:55:39 -0500

    Refactor: separar procesamiento y scraping en nuevas carpetas

---
+++ src/pipleline.py
@@ -0,0 +1,22 @@
+"""
+Pipeline principal del proyecto Ubika
+Flujo:
+Scraping → Processing → Preprocessing → Load
+"""
+
+# -------------------------
+# IMPORTS
+# -------------------------
+
+
+# -------------------------
+# PIPELINE
+# -------------------------
+def run_pipleline():
+    print("🚀 Iniciando pipeline Ubika...\n")
+
+# -------------------------
+# ENTRY POINT
+# -------------------------
+if __name__ == "__main__":
+    run_pipleline()
