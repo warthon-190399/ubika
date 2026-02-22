3634ad94a3361dbaf3d895b8580ce3c612187f11
Jimmy Warthon <jim.warthon@gmail.com>
2026-02-22 13:51:03 -0500

    Se actulizan archivos

--- src/pipleline.py
+++ src/pipleline.py
@@ -7,13 +7,17 @@ Scraping → Processing → Preprocessing → Load
 # -------------------------
 # IMPORTS
 # -------------------------
-
+from data.scraping import scraper_adondevivir
+from data.scraping import scraper_adondevivir_detalles
+from data.scraping import scraper_properati
 
 # -------------------------
 # PIPELINE
 # -------------------------
 def run_pipleline():
     print("🚀 Iniciando pipeline Ubika...\n")
+    raw_adondevivir = scraper_adondevivir()
+
 
 # -------------------------
 # ENTRY POINT
